9009cf6d38eacd83f5fcf887e6e8ad5ddb1c3073
JianyiXian <jenniferxian123@gmail.com>
2023-09-20 23:25:59 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Greeting from './component/Greeting.js';
+
+function App() {
+  return (
+    <div>
+      <Greeting />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Greeting.js
@@ -0,0 +1,7 @@
+const Greeting = () => {
+    return (
+        <h1>Hello World</h1>
+    )
+}
+
+export default Greeting;
